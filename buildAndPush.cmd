93f52a5a96ab134379b955ace79c5d02598da2da
Eusthace Corin <juanluis.suarez@gmail.com>
2025-02-01 17:47:33 +0000

    Docker client and server

---
+++ buildAndPush.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+@REM gcloud auth configure-docker gcr.io
+
+@REM --no-cache
+docker build --no-cache -t noid-eliza .
+
+docker tag noid-eliza gcr.io/noid-one-development/noid-eliza:latest
+
+docker push gcr.io/noid-one-development/noid-eliza:latest
