6ddbab846788e15e5f2042d97622f56d340c49c5
Eusthace Corin <juanluis.suarez@gmail.com>
2025-02-13 22:06:50 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ cd /app || exit
 echo "Starting backend service..."
 pnpm start --characters=characters/character.json &
 
-sleep 5  # Ensure backend starts
+sleep 60
 
 echo "Starting frontend service..."
 pnpm start:client --host &
 
-sleep 5  # Ensure frontend starts
+sleep 60
 
 echo "Starting Caddy..."
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
